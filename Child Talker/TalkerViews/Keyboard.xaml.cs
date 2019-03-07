12718753fc6e80e484b22a0a1c2b6a898c0798d8
sam21marchant <32226639+sam21marchant@users.noreply.github.com>
2019-03-07 17:34:16 -0500

    Merge Keyboard fixes back into DEV (#1)
    * Squashed some bugs in the keyboard
    
    The space bar and backspace weren't working properly. Just changed a couple lines of code to get them squared away.
    
    * Edit to Button_Click
    
    Button_Click is now implemented using a case switch

--- Child Talker/TalkerViews/Keyboard.xaml.cs
+++ Child Talker/TalkerViews/Keyboard.xaml.cs
@@ -54,34 +54,31 @@ namespace Child_Talker.TalkerViews
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string s = (sender as Button).Content.ToString();
+          
+            switch (s)
+            {
+                case "SPACE":
+                    sb.text += " ";
+                    break;
+                case "BACK":
+                    try { 
+                        sb.text = sb.text.Substring(0, sb.text.Length - 1);
+                    }
+                    catch (System.ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("There are no characters to delete!");
+                    }
+                    break;
+                case "ENTER":
+                    EnterPress();
+                    break;
+                default:
+                    sb.text += s;
+                    break;
 
-            if (s.Equals("ENTER"))
-            {
-                EnterPress();
-                return;
             }
-            else if (s.Equals("SPACE"))
-            {
-                greetingOutput.Text += " ";
-                return;
-            }
-            else if (s.Equals("BACK"))
-            {
-                try
-                {
-                    greetingOutput.Text = greetingOutput.Text.Substring(0, greetingOutput.Text.Length - 1); //deletes a character
-                }
-                catch (System.ArgumentOutOfRangeException)
-                {
-                    Console.WriteLine("There are no characters to delete!");
-                }
-                Console.WriteLine(sb.text);
-                return;
-            }
-
-            sb.text += s;
+            
             greetingOutput.Text = sb.text;
-            Console.WriteLine(sb.text);
         }
 
         private void EnterPress()
