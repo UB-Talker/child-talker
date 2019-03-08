a4f89f4fc53371f972f5664f42dd2c7935b370f7
Sam Marchant <sam17marchant@gmail.com>
2019-03-08 17:18:05 -0500

    Added Utilities Folder
    This will store all of the classes that will help the behind the scenes magic happen

---
+++ Child Talker/Utilities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Child_Talker.Utilities
+{
+    class Class1
+    {
+    }
+}
