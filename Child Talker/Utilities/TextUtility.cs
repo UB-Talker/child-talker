becc2f1e6949502ef918f39b60a7da83de5f514b
Sam Marchant <sam17marchant@gmail.com>
2019-03-28 17:26:32 -0400

    Tokenize strings when spoken
    TextUtility will now tokenize the spoken strings and store each word in a map as a key-value pair with the word being the key and the total number of times it was ever spoken as the value. Currently words that are the same but have different cases are treated the same i.e. "hello" and "HELLO" are considered to be equivalent.

--- Child Talker/Utilities/TextUtility.cs
+++ Child Talker/Utilities/TextUtility.cs
@@ -15,13 +15,21 @@ namespace Child_Talker.Utilities
 
         //Instance variables
         private List<Tuple<DateTime, string>> spokenPhrases;
+        private Dictionary<string, int> wordCounts;
         private SpeechSynthesizer synth;
 
         private TextUtility()
         {
             //Loads the saved list of speech history from SpeechHistory.txt once the application launches.
-            string text = System.IO.File.ReadAllText("../../Utilities/SpeechHistory.txt");
-            spokenPhrases = JsonConvert.DeserializeObject<List<Tuple<DateTime, string>>>(text);
+            string spokenPhrasesText = System.IO.File.ReadAllText("../../Utilities/SpeechHistory.txt");
+            string wordCountText = System.IO.File.ReadAllText("../../Utilities/WordCount.txt");
+            spokenPhrases = JsonConvert.DeserializeObject<List<Tuple<DateTime, string>>>(spokenPhrasesText);
+            wordCounts = JsonConvert.DeserializeObject<Dictionary<string, int>>(wordCountText);
+
+            if(wordCounts == null)
+            {
+                wordCounts = new Dictionary<string, int>();
+            }
 
             synth = new SpeechSynthesizer();
         }
@@ -46,6 +54,9 @@ namespace Child_Talker.Utilities
 
         /*
          * Speaks and saves the given input text with a timestamp of when it was spoken.
+         * The spoken string is also tokenized and stored in wordCounts. If the string
+         * already exists as a key in wordCounts, its mapped value will be incremented by one.
+         * Otherwise it will be added to wordCounts with an initial value of 1.
          */
         public void speak(string text)
         {
@@ -53,17 +64,36 @@ namespace Child_Talker.Utilities
             spokenPhrases.Add(phraseData);
 
             synth.SpeakAsync(text);
+
+            string[] words = text.Split(' ');
+            
+            for(int i  = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i].ToLower();
+                if (wordCounts.ContainsKey(currentWord))
+                {
+                    wordCounts[currentWord] += 1;
+                }
+                else
+                {
+                    wordCounts.Add(currentWord, 1);
+                }
+            }
         }
 
 
         /*
          * !!!!!! USE ONLY WHEN THE WINDOW IS CLOSING !!!!!!!!!!
          * Writes contents of spokenPhrases to SpeechHistory.txt
+         * Writes contents of wordCounts to WordCount.txt
          */
         public void save()
         {
-            string data = JsonConvert.SerializeObject(spokenPhrases);
-            System.IO.File.WriteAllText("../../Utilities/SpeechHistory.txt", data);
+            string speechHistoryData = JsonConvert.SerializeObject(spokenPhrases);
+            System.IO.File.WriteAllText("../../Utilities/SpeechHistory.txt", speechHistoryData);
+
+            string wordCountData = JsonConvert.SerializeObject(wordCounts);
+            System.IO.File.WriteAllText("../../Utilities/WordCount.txt", wordCountData);
         }
 
     }
