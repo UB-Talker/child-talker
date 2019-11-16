7826886d375faafcbc1db0be131d83399078ed38
sam21marchant <32226639+sam21marchant@users.noreply.github.com>
2019-11-16 16:53:58 -0500

    Migrated ButtonReferences (#69)
    Migrated references to to autoscan singleton to function definition. 
    localized onAction event to function definition of Remove_VOL_popup

--- Child Talker/TalkerViews/EnvControlsPage/Remote_VOL_popup.xaml.cs
+++ Child Talker/TalkerViews/EnvControlsPage/Remote_VOL_popup.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Windows.Controls;
+using Child_Talker.Utilities;
 
 namespace Child_Talker.TalkerViews.EnvControlsPage
 {
@@ -8,11 +9,15 @@ namespace Child_Talker.TalkerViews.EnvControlsPage
     /// </summary>
     public partial class Remote_VOL_popup : SecondaryWindow
     {
+        Utilities.Autoscan2 scan;
         EnvControls parent;
         public Remote_VOL_popup(EnvControls _parent)
         {
             InitializeComponent();
             parent = _parent;
+            BackButton.Click += ((bSender, bE) => { this.Close(); });
+            scan = Utilities.Autoscan2.Instance;
+            scan.ClearReturnPointList();
         }
 
         private void TV_controls(object sender, RoutedEventArgs e)
