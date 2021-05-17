038a8ade49e34d7e66d5b22060b590ba2b3a947f
iankamin <iankamin@buffalo.edu>
2021-05-17 19:44:44 -0400

    improved autoscan on phrases page. increased suggestions when pressing backspace to match number of suggestions when inputting character

---
+++ Child Talker/ChildTalkerTile/IChildTalkerTile-Razer.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms.VisualStyles;
+
+namespace Child_Talker
+{
+    public interface IChildTalkerTile
+    {
+        bool IsLink();
+        void PerformAction();
+        string Text { get; set; }
+        string ImagePath { get; set; }
+        IChildTalkerTile Parent { get; set; }
+        ChildTalkerXml Xml { get; set; }
+        bool InColor { get; set; }
+
+    }
+}
+
